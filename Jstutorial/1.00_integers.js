e8c7440bd887681b4e2421335ad889765cafbb40
D. Davis <D. Davis>
2016-07-15 19:34:25 -0400

    integers

--- Jstutorial/1.00_integers.js
+++ Jstutorial/1.00_integers.js
@@ -1,4 +1,7 @@
-//Integers are any numbers that are written with or without decimals
+//Integers//
 
-var x = 23;
-var y = 2.14;
+//An integer is a number that has no fractional part, and no digits after
+//the decimal point. An integer can be positive, negative or zero
+//Examples of intergers:
+
+9 , 22 , 0 , 50, 1000, etc
