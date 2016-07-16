872a272bb3b3d4dd709ce050fe85d1a9a51ec13d
D. Davis <D. Davis>
2016-07-15 20:01:47 -0400

    strings

--- Jstutorial/1.10_strings.js
+++ Jstutorial/1.10_strings.js
@@ -1,8 +1,27 @@
-//a string is any text inside a of '' or double ""
+//A string can be any text inside double or single quotes. It does not have to end with a semicolon
+//but this is good practice
 
-var myName = "Danyel";
-var myName = 'Danyel';
+var firstName = 'Danyel';
+
+//This will return: Danyel. 
+console.log(firstName);
+
+//Adding more than one string together is called a concatenation
+
+var firstName = 'Danyel';
+var lastName = 'Davis';
+
+//This will return Danyel Davis and adding the '' allows for a space in between the text
+console.log(firstName + ' ' + lastName);
+
 
 //if you have an apostrophe in your string, the code will break, so do the following instead
 
 var n = 'Danyel\'s dog is the cutest';
+console.log(n);
+
+
+
+
+
+
