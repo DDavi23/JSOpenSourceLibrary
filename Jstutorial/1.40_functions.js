a10f33133be1cb82e2e1bea8f6b1115283aca30f
D. Davis <D. Davis>
2016-07-15 21:32:21 -0400

    functions

--- Jstutorial/1.40_functions.js
+++ Jstutorial/1.40_functions.js
@@ -2,9 +2,13 @@
 //we do not have to define the function before we use it
 //the code for a function MUST be contained by curly brackets {}
 
-hello();
-function hello() {
-document.write('Hello');
+
+function turtleFact() {
+	console.log("A turtle\'s lower shell is called a plastron.");
 }
+         
+            
+//Once you declare the function, you can you turtleFact(); as many times as you want
 
-
+turtleFact();
+//line 13 will print out the fact about the turtle
