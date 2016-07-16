b421453e883c148e9c5e08f3101d7ee16b5eaf3c
D. Davis <D. Davis>
2016-07-15 20:19:02 -0400

    booleans

--- Jstutorial/1.20_booleans.js
+++ Jstutorial/1.20_booleans.js
@@ -1,7 +1,13 @@
-//represents to values...false or true
+//a statement that can have two values, true or false
 
-(10<9)  //returns a value of false
-(11>8) //returns of value of true
+10 < 9;  //returns a value of false
+11 > 8; //returns of value of true
 
+var yearsLivedInIndiana = 28
 
+if (yearsLivedInIndiana > 5){
+	console.log("It\s time to move!")
+}
+
+//The statement will print because it is true
 
