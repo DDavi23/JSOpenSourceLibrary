34d960f93890bd3f2f4a8a17529ef5f34fb26eeb
D. Davis <D. Davis>
2016-07-17 19:54:20 -0400

    booleans amended

--- Jstutorial/1.20_booleans.js
+++ Jstutorial/1.20_booleans.js
@@ -1,4 +1,9 @@
 //a statement that can have two values, true or false
+//the words true and false do not need to be in quotes because they are reserved words
+var danyelEats=true;
+var danyelExercises=false;
+
+//you can also use the number 1 for true and the number 2 for false
 
 console.log(10 < 9);  //returns a value of false
 console.log(11 > 8); //returns of value of true
