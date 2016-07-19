57eaeabf7661e5025a23e453e773fc032fe71c5c
D. Davis <D. Davis>
2016-07-18 20:35:43 -0400

    added more to functions

--- Jstutorial/1.40_functions.js
+++ Jstutorial/1.40_functions.js
@@ -1,8 +1,10 @@
-// A function is a set of JavaScript statements put together for a single purpose
-//we do not have to define the function before we use it
-//the code for a function MUST be contained by curly brackets {}
-//To make use of the function, you will make a call to the function when you want to use it. 
-//You call a function by using the name, any parameters you want to send, and a semicolon, like this:
+/* A function is a set of JavaScript statements put together for a single purpose
+they help to organize the various parts of a script into the different tasks that must
+be accomplihed. They can also be used more than once
+we do not have to define the function before we use it
+the code for a function MUST be contained by curly brackets {}
+To make use of the function, you will make a call to the function when you want to use it. 
+You call a function by using the name, any parameters you want to send, and a semicolon, like this:*/
 
 function dosomething (mymoney, cost);
 
