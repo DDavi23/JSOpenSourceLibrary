17c22400414eb91441bdcc9e0af7208beffbea6f
D. Davis <D. Davis>
2016-07-25 16:03:58 -0400

    typeof

---
+++ Jstutorial/1.03_typeof.js
@@ -0,0 +1,22 @@
+//used to find the type of a JavaScript variable
+
+
+/* These are possible return values of typeof
+Undefined would return"undefined"
+Null would return "object" 
+Boolean would return "boolean"
+Number would return "number"
+String would return "string"
+Symbol would return "symbol"
+Function object would return "function"
+Any other object would return "object" */
+
+//returns a number
+typeof 100;
+
+//returns a string
+typeof "Danyel";
+
+//returns a boolean
+typeof false; 
+
