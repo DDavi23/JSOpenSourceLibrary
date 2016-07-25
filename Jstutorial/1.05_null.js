cdc724f05f08224f426f2b29d141812ebe3d4082
D. Davis <D. Davis>
2016-07-25 15:20:57 -0400

    null

---
+++ Jstutorial/1.05_null.js
@@ -0,0 +1,8 @@
+//null indicates the absence of value
+
+var bankAccount = null;
+var momsAccount = 34;
+
+ console.log(bankAccount); // prints out null
+ console.log(momsAccount); // prints out 34
+
