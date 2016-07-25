261dd592c0d385b9c254f58dfcb7df41b5ea5426
D. Davis <D. Davis>
2016-07-25 15:26:47 -0400

    undefined

---
+++ Jstutorial/1.04_undefined.js
@@ -0,0 +1,7 @@
+//means a variable has been declared but has not yet been assigned a value
+
+var bankAccount;
+
+console.log(bankAccount);
+
+ //this prints as undefined because a value was not assigned to the variable bankAccount
