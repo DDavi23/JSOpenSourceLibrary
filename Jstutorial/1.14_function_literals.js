2b2ddff737a2c89d0054b91a33e6523f54ed2289
D. Davis <D. Davis>
2016-07-26 14:54:10 -0400

    function literals

---
+++ Jstutorial/1.14_function_literals.js
@@ -0,0 +1,10 @@
+//has four parts. The first part is for the reserved word function
+//the second part, which is optional, is for the function's name
+//the third part are the set parameters
+//the fourth part, is the statement that is in between curly brackets
+
+var add = function (a, b) {
+ return(a + b);
+};
+ 
+console.log(add);
