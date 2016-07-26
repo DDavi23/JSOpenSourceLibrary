0d43399e42bec3f25a202a59f7cb17cf63f38a79
D. Davis <D. Davis>
2016-07-25 20:02:25 -0400

    constructors

---
+++ Jstutorial/1.75_constructors.js
@@ -0,0 +1,8 @@
+/*allows you to build sn object using the same basic syntax as a regular function
+The difference is where you place the code inside of the function and where you 
+access its contents*/
+
+function car () {//the constructor is defined
+	//properties go here
+}
+
